50b2ef9d06186d58ab408228ed8bef9b80f69f3f
wlsgud76 <gu9956@gmail.com>
2026-05-07 16:31:47 +0900

    chore(REQ-006): domain/__init__ 전면 수정

--- modules/doc-parser/domain/__init__.py
+++ modules/doc-parser/domain/__init__.py
@@ -5,21 +5,27 @@ domain 레이어 전체 public export
 
 사용 예시:
     from doc_parser.domain import (
-        Chunk, ChunkOverlapMeta,
+        Chunk, ChunkOverlapMeta, ChunkingStrategy,
         WarningInfo, ElapsedDetail,
-        QualityMetrics, QualityGateResult,
-        ParserPort,
-        ChunkingService, QualityGate, PIIMaskingService,
+        QualityMetrics, QualityGateResult, QualityConfig,
+        PIIMaskRule,
+        ParserPort, DocumentRepositoryPort, ConfigLoaderPort,
+        NormalizationService, ChunkingService, QualityGate,
+        PIIMaskingService, ParserFactory,
     )
 """
-from doc_parser.domain.entities.chunk import Chunk, ChunkOverlapMeta
+from doc_parser.domain.entities.chunk import Chunk, ChunkingStrategy, ChunkOverlapMeta
+from doc_parser.domain.entities.pii import PIIMaskRule
+from doc_parser.domain.entities.quality import QualityConfig, QualityGateResult, QualityMetrics
 from doc_parser.domain.entities.warning import ElapsedDetail, WarningInfo
+from doc_parser.domain.ports.config_port import ConfigLoaderPort
 from doc_parser.domain.ports.parser_port import ParserPort
+from doc_parser.domain.ports.repository_port import DocumentRepositoryPort
 from doc_parser.domain.services.chunking_service import ChunkingService
+from doc_parser.domain.services.normalizer import NormalizationService
+from doc_parser.domain.services.parser_factory import ParserFactory
 from doc_parser.domain.services.pii_masking_service import PIIMaskingService
 from doc_parser.domain.services.quality_gate import QualityGate
-from doc_parser.domain.value_objects.quality import QualityGateResult, QualityMetrics
-from doc_parser.domain.services.normalizer import Normalizer
 
 __all__ = [
     # entities/warning.py
@@ -28,15 +34,27 @@ __all__ = [
     # entities/chunk.py
     "ChunkOverlapMeta",
     "Chunk",
-    # value_objects/quality.py
+    "ChunkingStrategy",
+    # entities/quality.py
     "QualityMetrics",
     "QualityGateResult",
+    "QualityConfig",
+    # entities/pii.py
+    "PIIMaskRule",
     # ports/parser_port.py
     "ParserPort",
+    # ports/repository_port.py
+    "DocumentRepositoryPort",
+    # ports/config_port.py
+    "ConfigLoaderPort",
+    # services/normalizer.py
+    "NormalizationService",
     # services/pii_masking_service.py
     "PIIMaskingService",
     # services/chunking_service.py
     "ChunkingService",
     # services/quality_gate.py
     "QualityGate",
+    # services/parser_factory.py
+    "ParserFactory",
 ]
