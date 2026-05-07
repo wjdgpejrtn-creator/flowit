309b7732cfbf21a982659c5c374269f88702ad13
wlsgud76 <gu9956@gmail.com>
2026-05-07 16:29:04 +0900

    chore(REQ-006): adapters/parsers __init__ 수정

--- modules/doc-parser/adapters/parsers/__init__.py
+++ modules/doc-parser/adapters/parsers/__init__.py
@@ -7,13 +7,12 @@ from doc_parser.adapters.parsers.csv_parser import CsvParser
 from doc_parser.adapters.parsers.docx_parser import DocxParser
 from doc_parser.adapters.parsers.hwp_parser import HwpParser
 from doc_parser.adapters.parsers.hwpx_parser import HwpxParser
-from doc_parser.adapters.parsers.parser_factory import ParserFactory
+from doc_parser.adapters.parsers.markdown_parser import MarkdownParser
 from doc_parser.adapters.parsers.pdf_parser import PdfParser
 from doc_parser.adapters.parsers.pptx_parser import PptxParser
 from doc_parser.adapters.parsers.xlsx_parser import XlsxParser
 
 __all__ = [
-    "ParserFactory",
     "PdfParser",
     "DocxParser",
     "XlsxParser",
@@ -21,4 +20,5 @@ __all__ = [
     "PptxParser",
     "HwpParser",
     "HwpxParser",
+    "MarkdownParser",
 ]
